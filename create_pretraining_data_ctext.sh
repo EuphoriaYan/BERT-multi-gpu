0a53b4fe3d4a03eec042ad289d8de83d8d4fc90e
Euphoria Yan <ysq58000@foxmail.com>
2020-03-04 20:57:59 +0800

    Update codes.

--- create_pretraining_data_ctext.sh
+++ create_pretraining_data_ctext.sh
@@ -5,20 +5,7 @@ tfrecords/ctext.tfrecord2,\
 tfrecords/ctext.tfrecord3,\
 tfrecords/ctext.tfrecord4,\
 tfrecords/ctext.tfrecord5,\
-tfrecords/ctext.tfrecord6,\
-tfrecords/ctext.tfrecord7,\
-tfrecords/ctext.tfrecord8,\
-tfrecords/ctext.tfrecord9,\
-tfrecords/ctext.tfrecord10,\
-tfrecords/ctext.tfrecord11,\
-tfrecords/ctext.tfrecord12,\
-tfrecords/ctext.tfrecord13,\
-tfrecords/ctext.tfrecord14,\
-tfrecords/ctext.tfrecord15,\
-tfrecords/ctext.tfrecord16,\
-tfrecords/ctext.tfrecord17,\
-tfrecords/ctext.tfrecord18,\
-tfrecords/ctext.tfrecord19
+tfrecords/ctext.tfrecord6
 vocab_file=vocab.txt
 
 python create_pretraining_data.py \
